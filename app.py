0e7b1d20ecd8ade57fba79b413f2ff508b7426c6
okanolgun <oknolgn28@gmail.com>
2025-05-20 11:18:44 +0300

    model lfs ile yüklendi

---
+++ app.py
@@ -0,0 +1,17 @@
+# 2. app.py
+import gradio as gr
+from fastai.vision.all import *
+
+learn = load_learner("model.pkl")  # Hugging Face sunucusuna yüklenecek model
+
+labels = learn.dls.vocab
+
+def classify_image(img):
+    pred, idx, probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+gr.Interface(fn=classify_image,
+             inputs=gr.Image(type="pil"),
+             outputs=gr.Label(num_top_classes=3),
+             title="Intel Image Classifier",
+             description="Trained with FastAI on Intel Image Classification Dataset").launch()
